1cef7ab778edba4d646889a375bfb6003a4bab2e
c9 <3124495484@qq.com>
2026-06-26 19:01:45 +0800

    ci(release): add automated 1.7.2.2 release

--- build/publish_win-x64.cmd
+++ build/publish_win-x64.cmd
@@ -11,7 +11,7 @@ copy ..\tools\ffmpeg\win-x64\ffmpeg.exe ..\src\MPLR\bin\x64\Release\net9.0-windo
 copy ..\tools\ffmpeg\win-x64\ffprobe.exe ..\src\MPLR\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\
 del /s /q publish.7z
 7z a publish.7z ..\src\MPLR\bin\x64\Release\net9.0-windows10.0.26100.0\publish\win-x64\* -t7z -mx=5 -mf=BCJ2 -r -y
-for /f "usebackq delims=" %%i in (`powershell -NoLogo -NoProfile -Command "Get-Content '..\src\MPLR\MPLR.csproj' | Select-String -Pattern '<AssemblyVersion>(.*?)</AssemblyVersion>' | ForEach-Object { $_.Matches.Groups[1].Value }"`) do @set version=%%i
+for /f "usebackq delims=" %%i in (`powershell -NoLogo -NoProfile -Command "$props = [xml](Get-Content '..\Directory.Build.props'); $props.Project.PropertyGroup.VersionPrefix"`) do @set version=%%i
 del /s /q MPLR_v%version%_win-x64.7z
 makemica micasetup.json
 rename publish.7z MPLR_v%version%_win-x64.7z
